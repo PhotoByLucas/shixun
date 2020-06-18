b0b61a42c59473c90238bc9185595ac11cf861d5
232295311 <232295311@qq.com>
2020-06-19 00:28:50 +0800

    feat:内鬼大作战

--- SnippetHelloWorld.cpp
+++ SnippetHelloWorld.cpp
@@ -1,4 +1,5 @@
-﻿// Redistribution and use in source and binary forms, with or without
+﻿// 谁是内鬼大作战
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
 // are met:
 //  * Redistributions of source code must retain the above copyright
