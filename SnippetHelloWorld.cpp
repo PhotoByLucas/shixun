1caa9b9270448bd447c2d623acb992517160c5a6
forlawa <845422138@qq.com>
2020-06-21 02:49:38 +0800

    feat:新增地图2，地图3，补上了ccd碰撞检测

--- SnippetHelloWorld.cpp
+++ SnippetHelloWorld.cpp
@@ -70,8 +70,12 @@ PxPhysics*	gPhysics = NULL;
 PxDefaultCpuDispatcher*	gDispatcher = NULL;
 PxScene*	gScene = NULL;
 
-
+//球体，障碍材质
 PxMaterial*	gMaterial = NULL;
+
+//设置墙壁材质
+PxMaterial*	wallMaterial = NULL;
+
 
 PxPvd* gPvd = NULL;
 
@@ -87,18 +91,21 @@ std::vector<PxVec3> gContactImpulses;
 bool	gUseBinarySerialization = false;
 
 
-PxRigidDynamic* current = NULL;
-PxRigidDynamic* current1 = NULL;
+PxRigidDynamic* current = NULL;//左挡板
+PxRigidDynamic* current1 = NULL;//右挡板
 
-PxRigidStatic* stick1 = NULL;
-PxRigidStatic* stick2 = NULL;
-PxRigidStatic* stick3 = NULL;
-PxRigidStatic* box = NULL;
-PxRigidStatic* box1 = NULL;
-PxRigidStatic* box2 = NULL;
-PxRigidStatic* box3 = NULL;
-PxRigidDynamic* moveBox = NULL;
-
+PxRigidStatic* stick1 = NULL;//地图1中胶囊1
+PxRigidStatic* stick2 = NULL;//地图1中胶囊2
+PxRigidStatic* stick3 = NULL;//地图1中胶囊3
+PxRigidStatic* box = NULL; //地图1中正方体
+PxRigidStatic* box1 = NULL;//地图1中正方体1
+PxRigidStatic* box2 = NULL;//地图1中正方体2
+PxRigidStatic* box3 = NULL; //地图1中正方体3
+PxRigidDynamic* moveBox = NULL;//旋转棒
+PxRigidDynamic* moveBox1 = NULL;//旋转棒1
+PxRigidDynamic* moveBox2 = NULL;//旋转棒2
+PxRigidStatic* static_ptr = NULL;//六边形
+PxRigidStatic* static_ptr1 = NULL;//六边形
 PxReal stackZ = 10.0f;
 
 
@@ -118,7 +125,21 @@ PxVec3 test_barrier[] = {
 	PxVec3(15,10,sqrt(75)),
 	PxVec3(10,10,sqrt(300))
 };
+PxVec3 test_barrier1[] = {
+	PxVec3(0,0,0),
+	PxVec3(20,0,0),
+	PxVec3(-10,0,sqrt(300)),
+	PxVec3(0,0,sqrt(1200)),
+	PxVec3(30,0,sqrt(300)),
+	PxVec3(20,0,sqrt(1200)),
 
+	PxVec3(0,10,0),
+	PxVec3(20,10,0),
+	PxVec3(-10,10,sqrt(300)),
+	PxVec3(0,10,sqrt(1200)),
+	PxVec3(30,10,sqrt(300)),
+	PxVec3(20,10,sqrt(1200))
+};
 
 //根据凸多面体顶点创建
 PxConvexMesh* createConvexMesh(const PxVec3* verts, const PxU32 numVerts, PxPhysics& physics, PxCooking& cooking)
@@ -141,20 +162,36 @@ PxConvexMesh* createConvexMesh(const PxVec3* verts, const PxU32 numVerts, PxPhys
 	}
 	return convexMesh;
 }
-
+static PxFilterFlags filterShader(
+	PxFilterObjectAttributes attributes0,
+	PxFilterData filterData0,
+	PxFilterObjectAttributes attributes1,
+	PxFilterData filterData1,
+	PxPairFlags& pairFlags,
+	const void* constantBlock,
+	PxU32 constantBlockSize)
+{
+	pairFlags = PxPairFlag::eSOLVE_CONTACT;
+	pairFlags |= PxPairFlag::eDETECT_DISCRETE_CONTACT;
+	pairFlags |= PxPairFlag::eDETECT_CCD_CONTACT;
+	return PxFilterFlags();
+}
 PxFilterFlags ballFilterShader(
 	PxFilterObjectAttributes attributes0, PxFilterData filterData0,
 	PxFilterObjectAttributes attributes1, PxFilterData filterData1,
 	PxPairFlags& pairFlags, const void* constantBlock, PxU32 constantBlockSize)
 {
+	pairFlags = PxPairFlag::eSOLVE_CONTACT;
+	pairFlags |= PxPairFlag::eDETECT_DISCRETE_CONTACT;
+	pairFlags |= PxPairFlag::eDETECT_CCD_CONTACT;
 	// let triggers through
 	if (PxFilterObjectIsTrigger(attributes0) || PxFilterObjectIsTrigger(attributes1))
 	{
-		pairFlags = PxPairFlag::eTRIGGER_DEFAULT;
+		pairFlags |= PxPairFlag::eTRIGGER_DEFAULT;
 		return PxFilterFlag::eDEFAULT;
 	}
 	// generate contacts for all that were not filtered above
-	pairFlags = PxPairFlag::eCONTACT_DEFAULT;
+	pairFlags |= PxPairFlag::eCONTACT_DEFAULT;
 
 	// trigger the contact callback for pairs (A,B) where 
 	// the filtermask of A contains the ID of B and vice versa.
@@ -172,7 +209,7 @@ PxFilterFlags ballFilterShader(
 		isBall = false;
 		pairFlags |= PxPairFlag::eNOTIFY_TOUCH_FOUND;
 	}
-	return PxFilterFlag::eDEFAULT;
+	return PxFilterFlags();
 }
 
 static const PxFilterData collisionGroupBall(1, 1, 1, 1);
@@ -297,7 +334,7 @@ PxRigidDynamic* createBall(const PxTransform& t, const PxGeometry& geometry, con
 	//dynamicBall->setMass(1.0f);
 	//dynamic->addForce(PxVec3(1, 0, 0),physx::PxForceMode::eFORCE , true);
 	//dynamic->setAngularVelocity(velocity);
-	PxShape* dynamicBallShape = PxRigidActorExt::createExclusiveShape(*dynamicBall, geometry, *gMaterial);
+	PxShape* dynamicBallShape = PxRigidActorExt::createExclusiveShape(*dynamicBall, geometry, *wallMaterial);
 	dynamicBallShape->setSimulationFilterData(collisionGroupBall);//小球物碰撞标识
 	gScene->addActor(*dynamicBall);
 	isBall = true;
@@ -313,7 +350,7 @@ void removeBall() {
 void create_static(PxVec3 verts[], PxU32 size, PxVec3 globalpos) {
 	gCooking = PxCreateCooking(PX_PHYSICS_VERSION, *gFoundation,
 		PxCookingParams(PxTolerancesScale()));
-	PxRigidStatic* static_ptr = gPhysics->createRigidStatic(PxTransform(globalpos));
+	static_ptr = gPhysics->createRigidStatic(PxTransform(globalpos));
 	PxConvexMesh* mesh = createConvexMesh(verts, size, *gPhysics, *gCooking);
 	PxShape* shape = PxRigidActorExt::createExclusiveShape(*static_ptr, PxConvexMeshGeometry(mesh), *gMaterial);
 	//shape->setSimulationFilterData(PxFilterData(COLLISION_FLAG_DRIVABLE_OBSTACLE,
@@ -376,19 +413,77 @@ void createMap2() {
 	gScene->removeActor(*stick1);
 	gScene->removeActor(*stick2);
 	gScene->removeActor(*stick3);
-	PxShape* boxShape = gPhysics->createShape(PxBoxGeometry(10.0f, 10.0f, 10.0f), *gMaterial);
-	box = PxCreateStatic(*gPhysics, PxTransform(PxVec3(40.0f, 10.0f, 50.0f)), *boxShape);
-	gScene->addActor(*box);
-	box1 = PxCreateStatic(*gPhysics, PxTransform(PxVec3(-40.0f, 10.0f, 50.0f)), *boxShape);
-	gScene->addActor(*box1);
-	moveBox = createDynamic(PxTransform(PxVec3(150.0f, 4.0f, 80.0f)), PxBoxGeometry(4.0f, 4.0f, 4.0f), PxVec3(0.0f, 0.0f, 0.0f));
-	moveBox->setLinearVelocity(PxVec3(1.0f, 0.0f, 0.0f));
-	moveBox->setRigidDynamicLockFlags(
-		PxRigidDynamicLockFlag::eLOCK_LINEAR_Y |
-		PxRigidDynamicLockFlag::eLOCK_LINEAR_Z
-	);
-}
+	gScene->removeActor(*box2);
+	gScene->removeActor(*box3);
+	PxShape* capsuleShape = gPhysics->createShape(PxCapsuleGeometry(10.0f, 20.0f), *gMaterial);
+	//PxRigidStatic* stick0 = gPhysics->createRigidStatic(PxTransform(PxVec3(10.0f, 0.0f, -70.0f)));
+	PxTransform relativePose(PxQuat(PxHalfPi, PxVec3(0, 0, 1)));
+	capsuleShape->setLocalPose(relativePose);
+	capsuleShape->setSimulationFilterData(collisionGroupObstacle);//障碍物碰撞标识
+	//其他障碍
+	stick1 = PxCreateStatic(*gPhysics, PxTransform(PxVec3(0.0f, 0.0f, 0.0f)), *capsuleShape);
+	//stick->setRigidDynamicLockFlags(PxRigidDynamicLockFlag::eLOCK_LINEAR_Y);
+	gScene->addActor(*stick1);
+	//PxShape* boxShape = gPhysics->createShape(PxBoxGeometry(10.0f, 10.0f, 10.0f), *gMaterial);
+	//box = PxCreateStatic(*gPhysics, PxTransform(PxVec3(40.0f, 10.0f, 50.0f)), *boxShape);
+	//gScene->addActor(*box);
+	//box1 = PxCreateStatic(*gPhysics, PxTransform(PxVec3(-40.0f, 10.0f, 50.0f)), *boxShape);
+	//gScene->addActor(*box1);
+	moveBox = createDynamic(PxTransform(PxVec3(40.0f, 4.1f, 50.0f)), PxBoxGeometry(4.0f, 4.0f, 20.0f), PxVec3(0.0f, 0.0f, 0.0f));
+	moveBox->setAngularVelocity(PxVec3(0.0f, .5f, 0.0f));
+	moveBox->setAngularDamping(0.f);
+	moveBox->setActorFlag(PxActorFlag::eDISABLE_GRAVITY, true);
+	//moveBox->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, true);
+	PxRigidBodyExt::updateMassAndInertia(*moveBox, 100000.0f);
 
+	moveBox->setRigidDynamicLockFlags(
+		PxRigidDynamicLockFlag::eLOCK_LINEAR_Y|
+		PxRigidDynamicLockFlag::eLOCK_ANGULAR_X |
+		PxRigidDynamicLockFlag::eLOCK_ANGULAR_Z
+	);
+
+	moveBox1 = createDynamic(PxTransform(PxVec3(-50.0f, 4.1f, 0.0f)), PxBoxGeometry(4.0f, 4.0f, 20.0f), PxVec3(0.0f, 0.0f, 0.0f));
+	moveBox1->setAngularVelocity(PxVec3(0.0f, 1.f, 0.0f));
+	moveBox1->setAngularDamping(0.0f);
+	moveBox1->setActorFlag(PxActorFlag::eDISABLE_GRAVITY, true);
+	//moveBox->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, true);
+	PxRigidBodyExt::updateMassAndInertia(*moveBox1, 100000.0f);
+	moveBox1->setRigidDynamicLockFlags(
+		PxRigidDynamicLockFlag::eLOCK_LINEAR_Y |
+		PxRigidDynamicLockFlag::eLOCK_ANGULAR_X |
+		PxRigidDynamicLockFlag::eLOCK_ANGULAR_Z
+	);
+
+	moveBox2 = createDynamic(PxTransform(PxVec3(30.0f, 4.1f, -100.0f)), PxBoxGeometry(4.0f, 4.0f, 20.0f), PxVec3(0.0f, 0.0f, 0.0f));
+	moveBox2->setAngularVelocity(PxVec3(0.0f, .5f, 0.0f));
+	moveBox2->setAngularDamping(0.f);
+	moveBox2->setActorFlag(PxActorFlag::eDISABLE_GRAVITY, true);
+	//moveBox->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, true);
+	PxRigidBodyExt::updateMassAndInertia(*moveBox2, 100000.0f);
+
+	moveBox2->setRigidDynamicLockFlags(
+		PxRigidDynamicLockFlag::eLOCK_LINEAR_Y |
+		PxRigidDynamicLockFlag::eLOCK_ANGULAR_X |
+		PxRigidDynamicLockFlag::eLOCK_ANGULAR_Z
+	);
+	
+	
+}
+void createMap3() {
+	gScene->removeActor(*moveBox);
+	gScene->removeActor(*moveBox1);
+	gScene->removeActor(*moveBox2);
+	gScene->removeActor(*stick1);
+	//create_static(test_barrier, 12, PxVec3(-10, 0, -50));
+	create_static(test_barrier1, 12, PxVec3(35, 0, -50));
+	create_static(test_barrier1, 12, PxVec3(-35, 0, 0));
+	moveBox = createDynamic(PxTransform(PxVec3(40.0f, 4.0f, 50.0f)), PxBoxGeometry(4.0f, 4.0f, 20.0f), PxVec3(0.0f, 0.0f, 0.0f));
+	moveBox->setAngularVelocity(PxVec3(0.0f, .5f, 0.0f));
+	moveBox->setAngularDamping(0.f);
+	moveBox->setActorFlag(PxActorFlag::eDISABLE_GRAVITY, true);
+	//moveBox->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, true);
+	PxRigidBodyExt::updateMassAndInertia(*moveBox, 100000.0f);
+}
 void initPhysics(bool interactive)
 {
 	gFoundation = PxCreateFoundation(PX_FOUNDATION_VERSION, gAllocator, gErrorCallback);
@@ -406,7 +501,7 @@ void initPhysics(bool interactive)
 
 	sceneDesc.flags |= PxSceneFlag::eENABLE_CCD;
 	sceneDesc.filterShader = ballFilterShader;
-
+	
 	gDispatcher = PxDefaultCpuDispatcherCreate(2);
 	sceneDesc.cpuDispatcher	= gDispatcher;
 
@@ -420,14 +515,15 @@ void initPhysics(bool interactive)
 		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
 	}
 	gMaterial = gPhysics->createMaterial(0.5f, 0.98f, 0.98f);
-
+	wallMaterial= gPhysics->createMaterial(0.5f, 0.98f, 0.0f);
 
 	//创建四周围墙
 	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0,1,0,0), *gMaterial);
 	gScene->addActor(*groundPlane);
-	PxShape* wallShape1 = gPhysics->createShape(PxBoxGeometry(100.0f,5.0f,1.5f), *gMaterial);
-	wallShape1->setSimulationFilterData(collisionGroupSouthWall);//障碍物碰撞标识
-	PxRigidStatic* southWall = PxCreateStatic(*gPhysics, PxTransform(PxVec3(0.0f, 5.0f, 200.0f)), *wallShape1);
+	PxShape* wallShapeSpecial = gPhysics->createShape(PxBoxGeometry(100.0f,5.0f,1.5f), *gMaterial);
+	wallShapeSpecial->setSimulationFilterData(collisionGroupSouthWall);//障碍物碰撞标识
+	PxShape* wallShape1 = gPhysics->createShape(PxBoxGeometry(100.0f, 5.0f, 1.5f), *gMaterial);
+	PxRigidStatic* southWall = PxCreateStatic(*gPhysics, PxTransform(PxVec3(0.0f, 5.0f, 200.0f)), *wallShapeSpecial);
 	PxRigidStatic* northWall = PxCreateStatic(*gPhysics, PxTransform(PxVec3(0.0f, 5.0f, -200.0f)), *wallShape1);
 	PxShape* wallShape2 = gPhysics->createShape(PxBoxGeometry(1.5f, 5.0f, 200.0f), *gMaterial);
 	PxRigidStatic* eastWall = PxCreateStatic(*gPhysics, PxTransform(PxVec3(100.0f, 5.0f, 0.0f)), *wallShape2);
@@ -439,7 +535,7 @@ void initPhysics(bool interactive)
 
 
 
-	create_static(test_barrier, 12, PxVec3(-10, 0, 90));
+	//create_static(test_barrier, 12, PxVec3(-10, 0, 90));
 
 	//小墙做阻隔
 	PxShape* wallShape3 = gPhysics->createShape(PxBoxGeometry(1.0f, 5.0f, 160.0f), *gMaterial);
@@ -499,13 +595,13 @@ void initPhysics(bool interactive)
 	PxTransform localTm(offset);
 
 	//尝试创建右边摆臂
-	PxShape* rightHandWall1 = gPhysics->createShape(PxBoxGeometry(35.0f, 10.0f, 2.5f), *gMaterial);
+	PxShape* rightHandWall1 = gPhysics->createShape(PxBoxGeometry(35.0f, 10.0f, 2.5f), *wallMaterial);
 	//PxTransform relativePose1(PxQuat(PxHalfPi*0.33, PxVec3(0, 1, 0)));
 	rightHandWall1->setLocalPose(relativePose1);
 
 
 	PxRigidStatic* rightStaticStick = PxCreateStatic(*gPhysics, PxTransform(PxVec3(58.0f, 10.0f, 110.0f)), *rightHandWall1);
-	current = PxCreateDynamic(*gPhysics, PxTransform(PxVec3(90.0f, 30.0f, 95.0f))*localTm, PxBoxGeometry(16.0f, 10.0f, 1.0f), *gMaterial, 1.0f);
+	current = PxCreateDynamic(*gPhysics, PxTransform(PxVec3(90.0f, 30.0f, 95.0f))*localTm, PxBoxGeometry(16.0f, 10.0f, 1.0f), *wallMaterial, 1.0f);
 	(*createDampedD62)(rightStaticStick, PxTransform(PxVec3(-27.0f, 0.0f, 16.5f)), current, PxTransform(offset));
 	//current->setMass(30.0f);
 	current->setRigidBodyFlag(PxRigidBodyFlag::eENABLE_CCD, true);
@@ -523,12 +619,12 @@ void initPhysics(bool interactive)
 
 
 	//尝试创建左边摆臂
-	PxShape* leftHandWall1 = gPhysics->createShape(PxBoxGeometry(35.0f, 10.0f, 2.5f), *gMaterial);
+	PxShape* leftHandWall1 = gPhysics->createShape(PxBoxGeometry(35.0f, 10.0f, 2.5f), *wallMaterial);
 	//PxTransform relativePose1(PxQuat(PxHalfPi*0.33, PxVec3(0, 1, 0)));
 	leftHandWall1->setLocalPose(relativePose2);
 
 	PxRigidStatic* leftStaticStick = PxCreateStatic(*gPhysics, PxTransform(PxVec3(-70.0f, 10.0f, 110.0f)), *leftHandWall1);
-	current1 = PxCreateDynamic(*gPhysics, PxTransform(PxVec3(90.0f, 30.0f, 95.0f))*localTm, PxBoxGeometry(16.0f, 10.0f, 1.0f), *gMaterial, 1.0f);
+	current1 = PxCreateDynamic(*gPhysics, PxTransform(PxVec3(90.0f, 30.0f, 95.0f))*localTm, PxBoxGeometry(16.0f, 10.0f, 1.0f), *wallMaterial, 1.0f);
 	//current1->setMass(30.0f);
 
 
@@ -602,6 +698,7 @@ void keyPress(unsigned char key, const PxTransform& camera)
 	case 'I':   increaseScore(); break;
 	case 'U':   removeBall(); break;
 	case '2':   createMap2(); break;
+	case '3':   createMap3(); break;
 		/***default:
 	current->setActorFlag(PxActorFlag::eDISABLE_GRAVITY, false);
 	current->setAngularVelocity(PxVec3(0, 0, -10000), true);
